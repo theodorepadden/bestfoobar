f23f9e774af961c3227c185dbd996d9abb9c4396
Theo <theodorepadden@gmail.com>
2025-05-16 15:53:35 -0400

    custom generator size

--- generator.py
+++ generator.py
@@ -15,12 +15,22 @@ def writeToMain(line):
     with open("main.py", "a") as f:
         f.write(line + "\n")
 
+
+def setLargestNum(bigBoy = 1000):
+    try:
+        int(bigBoy)
+        return bigBoy
+    execpt:
+        print("Input a integer (ex 1, 2, 3) Running with default (1000)")
+        return 1000
+
+
 #clears to make sure file is empty
 with open("main.py", "w") as f:
     f.write("")
 
 #todo add way to pass arg for amount of ints generated
-largestNum = 100
+largestNum = setLargestNum(input("Enter the larget num here: "))
 writeToMain("i = 0")
 writeToMain("while i < 100:")
 
