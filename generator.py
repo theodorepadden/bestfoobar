04732c80ba84d311f9f2374fcbaada07ad6f6673
Theo <theodorepadden@gmail.com>
2025-05-16 15:18:54 -0400

    almost forgot this

--- generator.py
+++ generator.py
@@ -10,6 +10,18 @@ def checkBar(num):
     else:
         return False
 
+
+def writeToMain(line):
+    with open("main.py", "a") as f:
+        f.write(line)
+
+#clears to make sure file is empty
+with open("main.py", "w") as f:
+    f.write("")
+
+
+
+"""
 top = int(input("Enter the number you want to go up to: "))
 i = 0
 while i <= top:
@@ -25,7 +37,7 @@ while i <= top:
         returnString += str(i)
     print(returnString)
     i += 1
-
+"""
 
 
 
